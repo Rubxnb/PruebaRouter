12b46bd9de143c1f98de22be0a1388821d3fdfd3
Rubxnb <rubenbm198@gmail.com>
2023-12-08 13:55:21 +0100

    Updates

---
+++ assets/NuevaRuta-65d8f896.js
@@ -0,0 +1,1 @@
+import{G as r,ag as a}from"./index-5e13e06f.js";const s=(t,e)=>{const c=t.__vccOpts||t;for(const[n,o]of e)c[n]=o;return c},_={};function u(t,e){return r(),a("h1",null,"NuevaRuta")}const l=s(_,[["render",u]]);export{l as default};
